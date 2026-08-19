f2b1b615996682f9877e71a9cef7dadb56c75f7b
Edinson Madrid <edinsonmadrid@gmail.com>
2026-08-19 00:11:52 -0300

    prepara depaola para Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
-import { microfrontends } from '@vercel/microfrontends/experimental/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss(), microfrontends()],
+    plugins: [react(), tailwindcss() ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
